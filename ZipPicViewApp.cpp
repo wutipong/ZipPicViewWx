cba7c37cbe97ef5f8593e1aa91cf141d4d045c26
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2015-11-08 20:16:31 +0700

    Redirect Log to StdErr stream.

--- ZipPicViewApp.cpp
+++ ZipPicViewApp.cpp
@@ -7,8 +7,18 @@ wxIMPLEMENT_APP(ZipPicViewApp);
 
 bool ZipPicViewApp::OnInit() {
   wxInitAllImageHandlers();
+  log = new wxLogStderr;
+  wxLog::SetActiveTarget(log);
   auto frame = new MainFrame();
   frame->Show(true);
 
   return true;
+}
+
+int ZipPicViewApp::OnExit() {
+  wxApp::OnExit();
+  wxLog::SetActiveTarget(nullptr);
+  delete log;
+
+  return 0;
 }
--- ZipPicViewApp.h
+++ ZipPicViewApp.h
@@ -2,10 +2,15 @@
 #define __ZIP_PIC_VIEW_APP_H__
 
 #include <wx/wx.h>
+#include <wx/log.h>
 
 class ZipPicViewApp : public wxApp {
 public:
   virtual bool OnInit();
+  virtual int OnExit();
+
+private:
+  wxLog *log;
 };
 
 #endif
